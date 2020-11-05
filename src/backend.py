fc4a964564d612887c237264f3b1f7b946ec9ed0
shadingyu96 <294908720@qq.com>
2020-11-05 22:56:54 +0800

    数据存储

--- src/backend.py
+++ src/backend.py
@@ -1,8 +1,22 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://lagou3:lagou3@stuq.ceshiren.com:23306/lagou3'
+db = SQLAlchemy(app)
+
+
+# 数据库结构
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
 # @app.route('/')
@@ -18,7 +32,8 @@ class Main(Resource):
 # 用户管理
 class UserApi(Resource):
     def get(self):
-        return {'hello': 'world'}
+        users = User.query.all()
+        return [{'id': u.id, 'name': u.username} for u in users]
 
 
 # 用例管理
@@ -37,7 +52,6 @@ class TaskApi(Resource):
 class ReportApi(Resource):
     def get(self):
         return {'hello': 'world'}
-
 
 
 api.add_resource(Main, '/main')
