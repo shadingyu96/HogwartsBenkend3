b1d42691b3e1add685f9182009324f03a9ceb11d
shadingyu96 <294908720@qq.com>
2020-11-05 20:08:12 +0800

    脚手架建设

--- src/backend.py
+++ src/backend.py
@@ -1,10 +1,50 @@
 from flask import Flask
+from flask_restful import Resource, Api
+
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
 
+# @app.route('/')
+# def hello_world():
+#     return 'Hello, World!'
+
+# 用户管理
+class Main(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+
+# 用户管理
+class UserApi(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+
+# 用例管理
+class TestCaseApi(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+
+# 任务管理
+class TaskApi(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+
+# 报告管理
+class ReportApi(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+
+
+api.add_resource(Main, '/main')
+api.add_resource(UserApi, '/login')
+api.add_resource(TestCaseApi, '/testcase')
+api.add_resource(TaskApi, '/task')
+api.add_resource(ReportApi, '/report')
 
 if __name__ == '__main__':
     app.run(debug=True)
